bc6c777fc2737a83265989f7292977897bd7b987
hazardland <hazardland@gmail.com>
2017-03-17 20:07:56 +0400

    reView

--- config/localhost/database/default.php
+++ config/localhost/database/default.php
@@ -2,5 +2,6 @@
 
 	use \Core\Database;
 
-    Database::add (new PDO('mysql:host=127.0.0.1;dbname=test;charset=utf8','root',''), 'default');
-	Database::get('default')->setAttribute (\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    Database::add(new PDO('mysql:host=127.0.0.1;dbname=test;charset=utf8','root',''), 'default');
+    Database::get('default')->setAttribute (\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    Database::get('default')->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
