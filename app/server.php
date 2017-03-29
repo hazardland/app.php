b3caa4922140173c773ca75a508730c70eba9a4b
hazardland <hazardland@gmail.com>
2017-03-29 14:40:22 +0400

    added comment to server.php specifieng that user can define LIB path in server.php

--- app/server.php
+++ app/server.php
@@ -1,3 +1,14 @@
 <?php
 
+    /*
+        This file is included before everything.
+        After defining SERVER app.php will include
+        server specific config file.
+    */
     const SERVER = 'localhost';
+
+    /*
+        You can also specify here where is LIB folder on server
+        Or it will be set by app.php with default value
+    */
+    //define ('LIB',dirname(__DIR__).'/lib');
